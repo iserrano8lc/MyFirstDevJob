84b4d411501d6714223b810bbefc9e43b31e6f34
Ignacio Serrano <iserrano.8lc@gmail.com>
2021-10-15 21:45:09 -0500

    Creating close ssh script

---
+++ close_ssh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+printf "Retrieving SSH sessions\n"
+
+
+IFS=$'\n' read -rd '' -a SESSIONS <<< $(ps -C sshd -o pid,command | grep "pts/")
+
+
+printf "Active sessions: ${#SESSIONS[*]}\n"
+
+
+for i in ${!SESSIONS[@]}
+do
+        IFS=" " read -r -a SESSION <<< ${SESSIONS[i]}
+        printf "Closing session PID: ${SESSION[0]}  user: ${SESSION[2]}\n"
+        kill -9 ${SESSION[0]}
+done
+
+printf "Script finished\n"
