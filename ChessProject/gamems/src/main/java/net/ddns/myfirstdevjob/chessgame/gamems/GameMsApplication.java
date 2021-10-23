ce1a6b21aaed6e3fd5f2de350d2c2b637a322b25
Ignacio Serrano <iserrano.8lc@gmail.com>
2021-10-22 20:40:30 -0500

    Creating base files for Game MS development

---
+++ ChessProject/gamems/src/main/java/net/ddns/myfirstdevjob/chessgame/gamems/GameMsApplication.java
@@ -0,0 +1,13 @@
+package net.ddns.myfirstdevjob.chessgame.gamems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameMsApplication.class, args);
+	}
+
+}
---
+++ ChessProject/gamems/src/test/java/net/ddns/myfirstdevjob/chessgame/gamems/GameMsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ddns.myfirstdevjob.chessgame.gamems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
